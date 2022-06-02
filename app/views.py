21e7c579d82d9467a1cbe62b13e82aabe28a1b7e
Deepak Radhakrishnan <dpkrobomad@gmail.com>
2022-06-02 12:58:57 +0530

    added users api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import *
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    
